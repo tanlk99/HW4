44222e6443609260d6d366ce6173eed32f8a37ff
tanlk99 <tanlk99@gmail.com>
2015-05-30 21:55:22 +0800

    Fourth.2 Commit

---
+++ app/src/main/java/com/example/xtan/cepnet/ChatInterfaceActivity.java
@@ -0,0 +1,14 @@
+package com.example.xtan.cepnet;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class ChatInterfaceActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_interface);
+    }
+}
